c6e7a808396be558dbe2f381898cb667f488142b
geirmash <geirmash@gmail.com>
2019-02-16 20:15:27 +0100

    created project structure

---
+++ hackingtools/wordcrawler.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+ 
+##imports goes here
+
+"""
+Describe module
+TODO: Use classes
+"""
+def main():
+	"""Launcher."""
+	print('Hello!')
+ 
+if __name__ == "__main__":
+	main()
