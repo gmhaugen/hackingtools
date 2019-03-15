35ada0ea2521b4374bdfa4765160efbf986b9908
gmhaugen <geirmash@gmail.com>
2019-03-15 23:18:50 +0100

    Added some argument conditions.

--- hackingtools/portscanner.py
+++ hackingtools/portscanner.py
@@ -1,15 +1,61 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
  
-##imports goes here
+import socket
+import subprocess
+import sys
+import os
+import getopt
+from datetime import datetime
 
 """
-Describe module
-TODO: Use classes
+This module is a simple portscanner for fast recog in pentesting.
 """
-def main():
-	"""Launcher."""
-	print('Hello!')
- 
+HELPMESSAGE = 'helpmessage'
+
+def main(argv):
+    port = 0
+    verbose = False
+    dosweep = False
+    host = ''
+
+    try:
+        opts, args = getopt.getopt(argv, "hsp:")
+    except getopt.GetoptError:
+        print(HELPMESSAGE)
+        sys.exit()
+
+    for opt, arg in opts:
+        if opt == "-h":
+            print(HELPMESSAGE)
+        elif opt in ("-s", "--sweep"):
+            dosweep = True
+        elif opt == "-p":
+            port = arg
+        else:
+            print('Invalid input')
+            print(HELPMESSAGE)
+
+        if opt in ("-d", "--verbose"):
+            verbose = True
+
+        if opt in ("-z", "--ping"):
+            host = arg
+            ping_host(host)
+
+    if dosweep:
+        sweep_scan()
+
+
+def ping_host(ip):
+    print('Pinging host %s', ip)
+
+
+def sweep_scan():
+    print('Performing sweep scan')
+    response = os.open('ping -n 1 192.168.0.1')
+
+
+
 if __name__ == "__main__":
-	main()
+	main(sys.argv[1:])
